dcff1bac0e02bbc5d76b7f54fab69babeb4ddaff
SamuelHGerber <henrylightfoot@gmail.com>
2021-02-26 14:17:37 -0800

    C:/Program Files/Git/location passes test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,53 +9,69 @@ const client = require('../lib/client');
 describe('app routes', () => {
   describe('routes', () => {
     let token;
-  
+
     beforeAll(async done => {
       execSync('npm run setup-db');
-  
+
       client.connect();
-  
+
       const signInData = await fakeRequest(app)
         .post('/auth/signup')
         .send({
           email: 'jon@user.com',
           password: '1234'
         });
-      
+
       token = signInData.body.token; // eslint-disable-line
-  
+
       return done();
     });
-  
+
     afterAll(done => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    // test('returns animals', async() => {
 
-      const expectation = [
-        {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
-        },
-        {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
-        },
-        {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
-        }
-      ];
+    //   const expectation = [
+    //     {
+    //       'id': 1,
+    //       'name': 'bessie',
+    //       'coolfactor': 3,
+    //       'owner_id': 1
+    //     },
+    //     {
+    //       'id': 2,
+    //       'name': 'jumpy',
+    //       'coolfactor': 4,
+    //       'owner_id': 1
+    //     },
+    //     {
+    //       'id': 3,
+    //       'name': 'spot',
+    //       'coolfactor': 10,
+    //       'owner_id': 1
+    //     }
+    //   ];
+
+    //   const data = await fakeRequest(app)
+    //     .get('/animals')
+    //     .expect('Content-Type', /json/)
+    //     .expect(200);
+
+    //   expect(data.body).toEqual(expectation);
+    // });
+
+    test('returns name and coordinates of a city', async () => {
+
+      const expectation = {
+        "formatted_query": "Portland, Multnomah County, Oregon, USA",
+        "latitude": "45.5202471",
+        "longitude": "-122.6741949"
+      }
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/location')
         .expect('Content-Type', /json/)
         .expect(200);
 
